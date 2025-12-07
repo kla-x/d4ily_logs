c4b15cb39ac7fbbf79c55158c490397f5d6829a5
Emmanuel_Musau <musauem98@gmail.com>
2025-12-07 05:50:00 -0500

    make sequential requests with requests module

---
+++ scripting/networking/requests/requests_module.py
@@ -0,0 +1,53 @@
+import requests
+import time
+
+url = 'http://127.0.0.1:80/delay/'
+baseurl = 'http://127.0.0.1:80/'
+
+def timer_dec(finc):
+
+    def modded_func(*args, **kwargs):
+        
+        start = time.time()
+        finc(*args, **kwargs)
+        stop = time.time()
+        print('took: {} sec'.format(stop - start))
+
+    return modded_func
+
+
+@timer_dec
+def make_req(sleep_t):
+    option = '/delay/'
+    r  = requests.get(baseurl + option + str(sleep_t))
+    print(r.text)
+
+# @timer_dec
+def post_req(sleep_t, req_no):  
+    body = {"request_no ": req_no}
+    option = '/delay/'
+    p = requests.post(baseurl + option + str(sleep_t),json=body)
+    print(p.json().get('data'))
+
+
+# makes one get and post request with delay time as argument
+
+# make_req(.2) # tells server to delay for .2 sec before response
+# post_req(0.01, 5)
+
+#making 5000 requests, uniform delay of .01 sec
+
+@timer_dec
+def sequential_reqs(delay,req_count):
+    for i in range(req_count):
+        post_req(delay, i+1)
+    
+    print('made: {} requests'.format(req_count))
+
+
+# sequential_reqs(.01,500)
+
+
+
+
+
